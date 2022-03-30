5937e1a13f75e77a26ce48bcc1e919ca6345de7a
Carlos Schumacher <carlosdu.carloseduardo@gmail.com>
2022-03-30 20:48:36 -0300

    update

--- index.js
+++ index.js
@@ -1,10 +1,3 @@
-const { SerialPort } = require('serialport')
-const { ReadlineParser } = require('@serialport/parser-readline')
-const port = new SerialPort({ path: 'COM4', baudRate: 9600 })
-
-const parser = port.pipe(new ReadlineParser({ delimiter: '\r\n' }))
-parser.on('data', console.log)
-
 const express = require('express');
 const mongoose = require('mongoose');
 const requireDir = require('require-dir');
