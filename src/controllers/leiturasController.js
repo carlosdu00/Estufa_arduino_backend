e346965bf4020516504780d7bdb14b650887b7c3
carlosdu00 <61929315+carlosdu00@users.noreply.github.com>
2022-04-04 20:54:06 -0300

    Update leiturasController.js

--- src/controllers/leiturasController.js
+++ src/controllers/leiturasController.js
@@ -4,6 +4,7 @@ const Leitura = mongoose.model('Read');
 module.exports ={
     async insert (req,res){
         const leituras = await Leitura.create(req.body);
+        console.log(leituras);
         return res.json({leituras});
     },
     async index (req,res) {
